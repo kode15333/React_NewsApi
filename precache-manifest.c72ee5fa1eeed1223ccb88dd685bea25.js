9aea92bec79a1b362b9dd1b5951d3ed160b44c2f
Junho <kode15333@gmail.com>
2020-02-18 21:14:42 +0900

    Updates

---
+++ precache-manifest.c72ee5fa1eeed1223ccb88dd685bea25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc45c20e6f67c73e6387d86196e33a9c",
+    "url": "/React_NewsApi/index.html"
+  },
+  {
+    "revision": "87fdc512cceee97715e2",
+    "url": "/React_NewsApi/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3fe4ff74f9c2386b2157",
+    "url": "/React_NewsApi/static/js/2.9e446cdc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React_NewsApi/static/js/2.9e446cdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87fdc512cceee97715e2",
+    "url": "/React_NewsApi/static/js/main.a5c43311.chunk.js"
+  },
+  {
+    "revision": "427f8fc5505c16e73645",
+    "url": "/React_NewsApi/static/js/runtime-main.02445652.js"
+  }
+]);
